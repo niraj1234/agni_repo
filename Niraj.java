133e949281a4d95adda91226a41c5d29979c7e97
Raju3040 <raju.sinha3040@gmail.com>
2019-03-13 00:50:06 +0530

    Line added by Raju but no conflict

--- Niraj.java
+++ Niraj.java
@@ -5,5 +5,7 @@ public class Niraj{
     System.out.println("Welcome to GITHUB");
     System.out.println("Line added by Raju on Laptop-2")
     System.out.println("Line by Niraj on Laptop-1")
+    System.out.println("Added by Raju but no conflicting line")
+
    }
 }
